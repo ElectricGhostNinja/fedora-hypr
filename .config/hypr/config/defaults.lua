369e82e4429cb78ae1133964069842167acd54b3
ElectricGhostNinja <electricghostninja@tuta.io>
2026-09-19 10:32:23 -0400

    Initial dotfiles: hypr, shell, noctalia, gtk, theme configs

---
+++ .config/hypr/config/defaults.lua
@@ -0,0 +1,7 @@
+-- Hyprland default apps
+
+TERMINAL     = "wezterm"
+FILE_MANAGER = "yazi"
+BROWSER      = "firefox"
+EDITOR       = "emacs"
+CALCULATOR   = "gnome-calculator"
---
+++ .config/hypr/config/variables.lua
@@ -0,0 +1,8 @@
+-- Monitors
+MONITOR1 = "DP-2"
+MONITOR2 = ""
+MONITOR3 = ""
+PRIMARY_MONITOR = MONITOR1
+
+-- Workspaces
+NUM_WPM = 3 -- Number of workspaces per monitor (Max 10)
